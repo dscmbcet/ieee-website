339ffcfe91c068b8d86ae7d606289e581977971f
Advaith U <45172876+advaith-unnikrishnan@users.noreply.github.com>
2022-02-27 23:35:48 +0530

    Add keynote sessions

---
+++ assets/js/keynote.js
@@ -0,0 +1,79 @@
+function functionOne() {
+    var dots = document.getElementById("one-dot");
+    var moreText = document.getElementById("one-more");
+    var btnText = document.getElementById("one-btn");
+  
+    if (dots.style.display === "none") {
+      dots.style.display = "inline";
+      btnText.innerHTML = "Read more";
+      moreText.style.display = "none";
+    } else {
+      dots.style.display = "none";
+      btnText.innerHTML = "Read less";
+      moreText.style.display = "inline";
+    }
+  }
+
+  function functionTwo() {
+    var dots = document.getElementById("two-dot");
+    var moreText = document.getElementById("two-more");
+    var btnText = document.getElementById("two-btn");
+  
+    if (dots.style.display === "none") {
+      dots.style.display = "inline";
+      btnText.innerHTML = "Read more";
+      moreText.style.display = "none";
+    } else {
+      dots.style.display = "none";
+      btnText.innerHTML = "Read less";
+      moreText.style.display = "inline";
+    }
+  }
+
+  function functionThree() {
+    var dots = document.getElementById("three-dot");
+    var moreText = document.getElementById("three-more");
+    var btnText = document.getElementById("three-btn");
+  
+    if (dots.style.display === "none") {
+      dots.style.display = "inline";
+      btnText.innerHTML = "Read more";
+      moreText.style.display = "none";
+    } else {
+      dots.style.display = "none";
+      btnText.innerHTML = "Read less";
+      moreText.style.display = "inline";
+    }
+  }
+
+  function functionFour() {
+    var dots = document.getElementById("four-dot");
+    var moreText = document.getElementById("four-more");
+    var btnText = document.getElementById("four-btn");
+  
+    if (dots.style.display === "none") {
+      dots.style.display = "inline";
+      btnText.innerHTML = "Read more";
+      moreText.style.display = "none";
+    } else {
+      dots.style.display = "none";
+      btnText.innerHTML = "Read less";
+      moreText.style.display = "inline";
+    }
+  }
+
+  function functionFive() {
+    var dots = document.getElementById("five-dot");
+    var moreText = document.getElementById("five-more");
+    var btnText = document.getElementById("five-btn");
+  
+    if (dots.style.display === "none") {
+      dots.style.display = "inline";
+      btnText.innerHTML = "Read more";
+      moreText.style.display = "none";
+    } else {
+      dots.style.display = "none";
+      btnText.innerHTML = "Read less";
+      moreText.style.display = "inline";
+    }
+  }
